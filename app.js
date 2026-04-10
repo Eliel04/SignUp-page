3afd1648aaeb89a1d5028c8d27fce489cc69a188
Eliel <kayembemukulu@gmail.com>
2026-04-10 16:34:41 -0500

    saving user info to localStorage

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const signupForm = document.getElementById('signupForm');
-const inputFields = signupForm.querySelectorAll('input');
+const form = document.getElementById('form');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
 
-signupForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+    const userInfo = {        // get info from form and create an object
+        id: Date.now(),      // generate a unique id based on the current timestamp
+        username: username,
+        password: password,
+        email: email
+    };
+
+
+    //save to local storage
+    localStorage.setItem('userInfo', JSON.stringify(userInfo));
+    console.log('User information saved to local storage:', userInfo);
+    // Optionally, you can clear the form after submission
+    form.reset();
 });
